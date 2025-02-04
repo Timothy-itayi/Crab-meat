1ef88a3ea18be872e4552df3a11ac865f4a7a352
Timothy <timmytam10@gmail.com>
2025-02-04 19:46:21 +1100

    Now it is time to be generic

--- lists/target/debug/deps/your_crate_name-47d8c3fa79584fea.d
+++ lists/target/debug/deps/your_crate_name-47d8c3fa79584fea.d
@@ -1,8 +1,9 @@
-/Users/user/Desktop/Crab-meat/lists/target/debug/deps/libyour_crate_name-47d8c3fa79584fea.rmeta: src/lib.rs src/first.rs
+/Users/user/Desktop/Crab-meat/lists/target/debug/deps/libyour_crate_name-47d8c3fa79584fea.rmeta: src/lib.rs src/first.rs src/second.rs
 
-/Users/user/Desktop/Crab-meat/lists/target/debug/deps/libyour_crate_name-47d8c3fa79584fea.rlib: src/lib.rs src/first.rs
+/Users/user/Desktop/Crab-meat/lists/target/debug/deps/libyour_crate_name-47d8c3fa79584fea.rlib: src/lib.rs src/first.rs src/second.rs
 
-/Users/user/Desktop/Crab-meat/lists/target/debug/deps/your_crate_name-47d8c3fa79584fea.d: src/lib.rs src/first.rs
+/Users/user/Desktop/Crab-meat/lists/target/debug/deps/your_crate_name-47d8c3fa79584fea.d: src/lib.rs src/first.rs src/second.rs
 
 src/lib.rs:
 src/first.rs:
+src/second.rs:
